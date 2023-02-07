9a11f1c13d22c3b740ff35be997764a684942ab6
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-08 04:52:05 +0530

    okkkk

--- src/components/Githubcal.jsx
+++ src/components/Githubcal.jsx
@@ -1,34 +1,22 @@
-// import "./Github.css";
 
 export default function Githubcal() {
   return (
     <div className="gitcont">
-      <h1 align="center" style={{ fontSize: "3rem", marginBottom: "50px" }}>
+      <h1  style={{ fontSize: "3rem", marginBottom: "50px" }}>
         Github
       </h1>
-      <img style={{width:"100%"}}
-        align="center"
-        src="https://github-readme-stats-sigma-five.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=gotham"
-        alt="abu1osama"
-      />
-      <p>
-        <img  style={{width:"100%"}}
-          align="center"
+     
+
+      <img id="github-top-langs"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api/top-langs?username=Abu1Osama&show_icons=true&locale=en&layout=compact" alt="Abu1Osama" />
+      
+      <img id="github-stats-card"  style={{width:"100%"}} src="https://github-readme-stats-sigma-five.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=gotham" alt="abu1osama" />
+
+        <img id="github-streak-stats" style={{width:"100%"}}
           src="https://github-readme-streak-stats.herokuapp.com/?user=abu1osama&theme=gotham"
           alt="abu1osama"
         />
-      </p>
-      <p>
-        <a href="https://github.com/abu1osama">
-          <span>
-            <img  style={{width:"100%"}}
-              align="center"
-              src="https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=abu1osama&theme=monokai"
-              alt="img"
-            />
-          </span>
-        </a>{" "}
-      </p>
+
+       
     </div>
   );
 }
