cc78bc782c5a3fa7dc703e9027751fb9fd0911f3
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-08 09:49:55 +0530

    linked

--- src/components/Githubcal.jsx
+++ src/components/Githubcal.jsx
@@ -10,9 +10,9 @@ export default function Githubcal() {
      <Calendar />
      </div>
 
-      <img id="github-top-langs"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api/top-langs/?username=abu1osama&theme=default&layout=compact" alt="Abu1Osama" />
+      <img id="github-top-langs"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api/top-langs/?username=abu1osama&theme=default&layout=compact" alt="abu1Osama" />
       
-      <img id="github-stats-card"  style={{width:"100%"}} src="https://github-readme-stats-sigma-five.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=default&layout=compact" alt="abu1osama" />
+      <img id="github-stats-card"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=default&layout=compact" alt="abu1osama" />
 
         <img id="github-streak-stats" style={{width:"100%"}}
           src="https://github-readme-streak-stats.herokuapp.com?user=abu1osama&theme=default&layout=compact"
