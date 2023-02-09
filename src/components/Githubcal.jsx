feaca64d240a4b24359c708033df72e0dcec8121
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-10 00:14:32 +0530

    osamamamamam

--- src/components/Githubcal.jsx
+++ src/components/Githubcal.jsx
@@ -1,25 +1,33 @@
-import Calendar from "./Calendar";
-
+import Githubcalendar from "react-github-calendar"
 export default function Githubcal() {
   return (
     <div className="gitcont">
-      <h1  style={{ fontSize: "3rem", marginBottom: "50px" }}>
-        Github
-      </h1>
-     <div>
-     <Calendar />
-     </div>
+      <h1 style={{ fontSize: "3rem", marginBottom: "50px" }}>Github</h1>
+      <div>
+      </div>
 
-      <img id="github-top-langs"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api/top-langs/?username=abu1osama&theme=default&layout=compact" alt="abu1Osama" />
-      
-      <img id="github-stats-card"  style={{width:"100%"}} src="https://github-readme-stats.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=default&layout=compact" alt="abu1osama" />
+      <img
+        id="github-top-langs"
+        style={{ width: "100%" }}
+        src="https://github-readme-stats.vercel.app/api/top-langs/?username=abu1osama&theme=default&layout=compact"
+        alt="abu1Osama"
+      />
 
-        <img id="github-streak-stats" style={{width:"100%"}}
-          src="https://github-readme-streak-stats.herokuapp.com?user=abu1osama&theme=default&layout=compact"
-          alt="abu1osama"
-        />
-        
-       
+      <img
+        id="github-stats-card"
+        style={{ width: "100%" }}
+        src="https://github-readme-stats.vercel.app/api?username=abu1osama&show_icons=true&locale=en&theme=default&layout=compact"
+        alt="abu1osama"
+      />
+
+      <img
+        id="github-streak-stats"
+        style={{ width: "100%" }}
+        src="https://github-readme-streak-stats.herokuapp.com?user=abu1osama&theme=default&layout=compact"
+        alt="abu1osama"
+      />
+
+      <Githubcalendar username="abu1osama" color="#16DB94"></Githubcalendar>
     </div>
   );
 }
