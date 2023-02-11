cd49cd9909156eeabca14a506fb452bbe26d0b2c
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-11 06:20:17 +0530

    sdsdd

--- src/components/Githubcal.jsx
+++ src/components/Githubcal.jsx
@@ -2,9 +2,11 @@ import Githubcalendar from "react-github-calendar"
 export default function Githubcal() {
   return (
     <div className="gitcont">
-      <h1 style={{ fontSize: "3rem", marginBottom: "50px" }}>Github</h1>
-      <div>
-      </div>
+      <h1 style={{ fontSize: "30px", marginBottom: "30px",textAlign:"center",fontWeight:"bold" }}>Github</h1>
+
+<Githubcalendar username="abu1osama" color="#16DB94" ></Githubcalendar>
+{/* <p  style={{ width: "120%",textAlign:"center",marginLeft:"25px",marginBottom:"20px" }}></p> */}
+      
 
       <img
         id="github-top-langs"
@@ -27,7 +29,6 @@ export default function Githubcal() {
         alt="abu1osama"
       />
 
-      <Githubcalendar username="abu1osama" color="#16DB94"></Githubcalendar>
     </div>
   );
 }
--- src/components/ProjectComponents.js
+++ src/components/ProjectComponents.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Projects from "./Projects.js";
 import "./ProjectComponents.scss";
-import Trip7 from "./Projects-pic/Trip7.png";
-import Trip12 from "./Projects-pic/Trip12.jpeg";
-import Trip9 from "./Projects-pic/Trip9.png";
-import Trip10 from "./Projects-pic/trip10.jpeg";
-import zara11 from "./Projects-pic/zara11.jpeg";
-import zara12 from "./Projects-pic/zara12.jpeg";
-import zara13 from "./Projects-pic/zara13.jpeg";
-import zara19 from "./Projects-pic/zara19r.jpeg";
-import shop12 from "./Projects-pic/shop12.jpeg";
-import shop14 from "./Projects-pic/shop14.jpeg";
-import shop15 from "./Projects-pic/shop15.jpeg";
-import shop17 from "./Projects-pic/shop17.jpeg";
+import A1 from "./Projects-pic/A1.png"
+import A2 from "./Projects-pic/A2.png"
+import A3 from "./Projects-pic/A3.png"
+import A4 from "./Projects-pic/A4.png"
+
+import B1 from "./Projects-pic/B1.png"
+import B2 from "./Projects-pic/B2.png"
+import B3 from "./Projects-pic/B3.png"
+import B4 from "./Projects-pic/B4.png"
+
+import C1 from "./Projects-pic/C1.png"
+import C2 from "./Projects-pic/C2.png"
+import C3 from "./Projects-pic/C3.png"
+import C4 from "./Projects-pic/C4.png"
+
 import movie1 from "./Projects-pic/movie1.png";
 import movie2 from "./Projects-pic/movie2.png";
 import movie3 from "./Projects-pic/movie3.png";
@@ -21,7 +24,8 @@ import movie4 from "./Projects-pic/movie4.png";
 export default function ProjectComponents() {
   const project1 = ["fab fa-html5", "fab fa-css3-alt", "fab fa-js"];
   const project2 = ["fab fa-html5", "fab fa-css3-alt", "fab fa-js"];
-  const project3 = ["fab fa-html5", "fab fa-css3-alt", "fab fa-js"];
+  const project3 = ["fab fa-html5", "fab fa-css3-alt", "fab fa-js","fab fa-react"];
+  const project4 = ["fab fa-html5", "fab fa-css3-alt", "fab fa-js","fab fa-react"];
 
   // let object_img = {
   //   img1: Trip,
@@ -33,15 +37,15 @@ export default function ProjectComponents() {
     {
       title: "Tripadivisor_Clone",
       description:
-        "In this project we have tried to manipulate dom elements, styling using css and tried to make website more dynamic and responsive.some of the functionality we have added are sign-in/sign-up, home page, user authentication, payment page,  developed data using local storage.",
+        "In this project I have tried to manipulate dom elements, styling using css and tried to make website more dynamic and responsive.some of the functionality I have added are sign-in/sign-up, home page, user authentication, payment page,  developed data using local storage.",
       arr:  project1,
       github: "https://github.com/Abu1Osama/august-wool-9387",
       website: "https://abu1osama.github.io/august-wool-9387/",
       object_img: {
-        img1: Trip7,
-        img2: Trip12,
-        img3: Trip9,
-        img4: Trip10,
+        img1: A1,
+        img2: A2,
+        img3: A3,
+        img4: A4,
       }
     },
     {
@@ -52,29 +56,29 @@ export default function ProjectComponents() {
       github: "https://github.com/i-m-prince/beneficial-jelly-4258",
       website: "https://tubular-concha-7a9bfb.netlify.app/",
       object_img: {
-        img1: zara11,
-        img2: zara12,
-        img3: zara13,
-        img4: zara19,
+        img1: B1,
+        img2: B2,
+        img3: B3,
+        img4: B4,
       },
     },
     {
       title:"Shopclues_Clone",
-      description:"In this project we have tried to manipulate dom elements, styling using css and tried to make website more dynamic and responsive.some of the functionality we have added are sign-in/sign-up, home page, user authentication, payment page,  developed data using local storage.",
+      description:"In this project we have tried to make a look alike clone of ShopClues.com, i have used React.Js & styling using css,chakra UI and tried to make website more dynamic and responsive. Data is developed using Json server.",
       arr:project3,
       github:"https://github.com/Abu1Osama/close-hand-543",
       website:"https://yousamuniverse-abu1osama.vercel.app/",
       object_img: {
-        img1: shop12,
-        img2: shop14,
-        img3: shop15,
-        img4: shop17,
+        img1: C1,
+        img2: C2,
+        img3: C3,
+        img4: C4,
       },
     },
     {
       title:"Movies_info",
-      description:"movies description",
-      arr:project3,
+      description:"Website  to search any movies including its trailer and teaser,i have made this project using React.js,Data is Developed using Api.",
+      arr:project4,
       github:"https://github.com/Abu1Osama/Movieapp_info",
       website:"https://movieapp-info.vercel.app/",
       object_img: {
