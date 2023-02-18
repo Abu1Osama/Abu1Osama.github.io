355ebd0a8722dbd20a172a53745826f014bf99b9
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-19 00:45:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
         <Navbar />
         <AboutMe />
         <MyDetails />
-        <Skills />
         <ProjectComponents />
+        <Skills />
         <Githubcal/>
         <Contact />
       </div>
