b37328b6c4e66c7cfd2159a0437b31671c7cbdfc
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2023-02-19 00:06:55 +0530

    jdbhcjkwfc

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,17 @@ import img from "./pic2.png";
 
 export default function Navbar() {
   const uncheck = () => {
-      setTimeout(() => {
-          document.getElementById("checker").checked = false;
+    setTimeout(() => {
+      document.getElementById("checker").checked = false;
     }, 1000);
   };
-  const resumedown=()=>{
-    setTimeout(()=>{
+  const resumedown = () => {
+    setTimeout(() => {
+      window.open("./Abu-resume.pdf", "_blank");
+    }, 5000);
 
-      window.open("./Abu-resume.pdf","_blank")
-    },5000)
-   
     // https://drive.google.com/file/d/1yAYMLTd488BqmOcYf3EYalF8DzxpEWfc/view?usp=sharing
-
-  }
+  };
 
   return (
     <nav id="nav-menu" className="container">
@@ -27,48 +25,60 @@ export default function Navbar() {
       </label>
       <ul>
         <li onClick={uncheck}>
-          {" "}
-          <i className="far fa-address-card"></i> <a className="nav-link home" href="#home"> Home </a>{" "}
+          <a className="nav-link home" href="#home">
+            {" "}
+            <i className="far fa-address-card"></i> Home{" "}
+          </a>{" "}
           <div></div>
         </li>
         <li onClick={uncheck}>
-          {" "}
-          <i className="far fa-address-card"></i> <a className="nav-link about" href="#about"> About </a>
+          <a className="nav-link about" href="#about">
+            {" "}
+            <i className="far fa-address-card"></i> About{" "}
+          </a>
           <div></div>
         </li>
         <li onClick={uncheck}>
-          {" "}
-          <i className="fas fa-sitemap"></i> <a className="nav-link skills" href="#skills"> Skills </a>
+          <a className="nav-link skills" href="#skills">
+            {" "}
+            <i className="fas fa-sitemap"></i> Skills{" "}
+          </a>
           <div></div>
         </li>
         <li onClick={uncheck}>
-          {" "}
-          <i className="fas fa-laptop"></i>{" "}
-          <a className="nav-link projects" href="#projects"> Projects </a>
+          <a className="nav-link projects" href="#projects">
+            {" "}
+            <i className="fas fa-laptop"></i> Projects{" "}
+          </a>
           <div></div>
         </li>
         <li onClick={uncheck}>
-          {" "}
-          <i className="fas fa-laptop"></i>{" "}
-          <a className="nav-link contact" href="#contact"> Contact </a>
+          <a className="nav-link contact" href="#contact">
+            {" "}
+            <i className="fas fa-laptop"></i> Contact{" "}
+          </a>
           <div></div>
         </li>
         <li id="resume-button-1" onClick={uncheck}>
-          {" "}
-          <button style={{background:"none",border: "none",fontWeight: "bold",fontSize:"16px"}} onClick={resumedown}>
-
-          <i className="far fa-file-alt"></i>
-          <a 
-          id="resume-link-1"
-          className="nav-link resume"
-          rel="noreferrer"
-          href="https://drive.google.com/uc?export=download&id=1yAYMLTd488BqmOcYf3EYalF8DzxpEWfc"
-          
+          <button
+            style={{
+              background: "none",
+              border: "none",
+              fontWeight: "bold",
+              fontSize: "16px",
+            }}
+            onClick={resumedown}
           >
-            {" "}
-            Resume{" "}
-          </a>{" "}
-            </button>
+            <a
+              id="resume-link-1"
+              className="nav-link resume"
+              rel="noreferrer"
+              href="https://drive.google.com/uc?export=download&id=1yAYMLTd488BqmOcYf3EYalF8DzxpEWfc"
+            >
+              <i className="far fa-file-alt"></i> 
+              Resume
+            </a>
+          </button>
           <div></div>
         </li>
       </ul>
